bec75869e98077fe70721fefacd81f8b052e7360
joaquinrapetti <j.rapetti92@gmail.com>
2022-04-22 14:58:29 -0300

    main responsive fixed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaInstagram, FaArrowDown } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Picture from "../assets/Picture.png";
 import "./MainStyles.css";
@@ -8,36 +8,46 @@ const Main = () => {
   return (
     <div className="main-container">
       <div className="left">
-          <h1>junior developer</h1>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting 
-              industry. Lorem Ipsum has been the industry's standard dummy 
-              text ever since the 1500s, when an unknown printer took a galley 
-              of type and scrambled it to make a type specimen book. 
-              It has survived not only five centuries, but also the leap into 
-              electronic typesetting, remaining essentially unchanged.</p>
+        <Link to="/about">Welcome! I'm</Link>
+        <h1>Joaquin Rapetti</h1>
+        <h4>Junior Developer.</h4>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged.
+        </p>
       </div>
       <div className="right">
         <img className="picture" src={Picture} alt="" />
       </div>
       <div className="social-bar">
-          <h4>Say hi and meet me</h4>
-        <ul>
-          <li>
-            <Link to="/">
-              <FaLinkedin size={30} style={{ color: "#f8f8f8" }} />
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <FaGithub size={30} style={{ color: "#f8f8f8" }} />
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <FaInstagram size={30} style={{ color: "#f8f8f8" }} />
-            </Link>
-          </li>
-        </ul>
+        <div className="social-content">
+          <h4>Say hi and meet me:</h4>
+          <ul>
+            <li>
+              <Link to="/">
+                <FaLinkedin size={30} style={{ color: "#d52d3f" }} />
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <FaGithub size={30} style={{ color: "#d52d3f" }} />
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <FaInstagram size={30} style={{ color: "#d52d3f" }} />
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="scroll-down">
+          <h5>Scroll Down</h5>
+          <FaArrowDown />
+        </div>
       </div>
     </div>
   );
